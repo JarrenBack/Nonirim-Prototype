6be6b01e8b9ad5181c010be02fbcac8b89142c17
Zachary Wolf <zlwdk4@mail.umkc.edu>
2016-02-16 22:56:56 -0600

    Zach first commit
    constructor for deck

--- Deck.h
+++ Deck.h
@@ -32,22 +32,88 @@ public:
 	 Deck() {
 		//here we could initialize the deck (push all of the cards on to the deck that it starts off with)
 		//So we will create of the needed cards and push them onto the stack
-		 Card blueKey(1, "key", "blue");
-		 Card greenSun(2, "sun", "green");
-		 Card brownMoon(3, "moon", "brown");
-		 Card greenSun2(4, "sun", "green");
-		 Card brownMoon2(5, "moon", "brown");
-		 Card blueKey2(6, "key", "blue");
-		 Card greenSun3(7, "sun", "green");
 
-		 addCardToDeck(blueKey);
-		 addCardToDeck(greenSun);
-		 addCardToDeck(brownMoon);
-		 addCardToDeck(greenSun2);
-		 addCardToDeck(brownMoon2);
-		 addCardToDeck(blueKey2);
-		 addCardToDeck(greenSun3);
 
+		 string sun = "sun" , moon = "moon", key = "key", blue = "blue", brown = "brown", green = "green", red = "red" , nightmare = "nightmare", door = "door";
+
+		 int i = 1;	
+		 for (int j = 0; i < 9; j++) {
+			 Card c(i, sun, red);
+			 addCardToDeck(c);
+			 i++;
+		 }
+
+		 for (int j = 0; i < 8; j++) {
+			 Card c(i, sun, blue);
+			 addCardToDeck(c);
+			 i++;
+		 }
+
+		 for (int j = 0; i < 7; j++) {
+			 Card c(i, sun, green);
+			 addCardToDeck(c);
+			 i++;
+		 }
+
+		 for (int j = 0; i < 6; j++) {
+			 Card c(i, sun, brown);
+			 addCardToDeck(c);
+			 i++;
+		 }
+		 
+		 for (int j = 0; i < 4; j++) {
+			 Card c(i, moon, red);
+			 i++;
+			 Card c1(i, moon, blue);
+			 i++;
+			 Card c2(i, moon, green);
+			 i++;
+			 Card c3(i, moon, brown);
+			 i++;
+			 addCardToDeck(c);
+			 addCardToDeck(c1);
+			 addCardToDeck(c2);
+			 addCardToDeck(c3);
+			 
+		 }
+
+		 for (int j = 0; i < 3; j++) {
+			 Card c(i, key, red);
+			 i++;
+			 Card c1(i, key, blue);
+			 i++;
+			 Card c2(i, key, green);
+			 i++;
+			 Card c3(i, key, brown);
+			 i++;
+			 addCardToDeck(c);
+			 addCardToDeck(c1);
+			 addCardToDeck(c2);
+			 addCardToDeck(c3);
+
+		 }
+
+		 for (int j = 0; j < 10; j++) {
+			 Card c(i, nightmare, nightmare);
+			 addCardToDeck(c);
+			 i++;
+		 }
+
+		 for (int j = 0; i < 2; j++) {
+			 Card c(i, door, red);
+			 i++;
+			 Card c1(i, door, blue);
+			 i++;
+			 Card c2(i, door, green);
+			 i++;
+			 Card c3(i, door, brown);
+			 i++;
+			 addCardToDeck(c);
+			 addCardToDeck(c1);
+			 addCardToDeck(c2);
+			 addCardToDeck(c3);
+
+		 }
 
 
 	}
